d1106d751070ddfe0020507dfb9fd31886fab484
dillon walsh <dillonwalsh93@gmail.com>
2021-06-25 12:24:41 -0400

    created engineer and intern object

--- 02-Challenge/Develop/lib/Engineer.js
+++ 02-Challenge/Develop/lib/Engineer.js
@@ -1,1 +1,17 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+
+class Engineer extends Employee {
+    constructor(name, id, email, gitHubName) {
+        super(name, id, email)
+        this.gitHubName = gitHubName;
+    }
+    gitHubName() {
+        return this.gitHubName
+    }
+    getRole() {
+
+        return "engineer"
+    }
+}
+
--- 02-Challenge/Develop/lib/Manager.js
+++ 02-Challenge/Develop/lib/Manager.js
@@ -14,8 +14,10 @@ class Manager extends Employee {
         return "manager"
     }
 }
-let Ted = new Manager ('Ted', 12,"ted@gmail", 'Manager', 1);
-console.log(Ted.name) //expecting Ted
+// test
+// let Ted = new Manager ('Ted', 12,"ted@gmail", 'Manager', 1);
+// console.log(Ted.name) 
+//expecting Ted
 
 //inquirer stuff, you'll get name, id, email, etc from the answers to the prompt 
 // as you define new Managers, new Interns, new Engineers, you will have access to that information by using the Ted.name syntax
