3317ab26cca8aa968065db199067930045f87518
dillon walsh <dillonwalsh93@gmail.com>
2021-06-27 11:41:59 -0400

    created final empty string

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const employees = [];
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
